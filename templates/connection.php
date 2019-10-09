a8188edcfefdc6c44c8b647983ed24346e47a767
Katherineslove <48259743+Katherineslove@users.noreply.github.com>
2019-10-10 09:17:13 +1300

    connected to the database

--- templates/connection.php
+++ templates/connection.php
@@ -2,6 +2,11 @@
     date_default_timezone_set('Pacific/Auckland');
     $dbc = mysqli_connect(getenv('DB_HOST'), getenv('DB_USERNAME'), getenv('DB_PASSWORD'), getenv('DB_TABLE'));
 
-
+    if($dbc){
+        // var_dump('we are connected');
+        $dbc->set_charset('utf8mb4');
+    } else {
+        die('ERROR, connection could not be made to the database, please check your envirnoment varables in your .env file. There is an example provided if you do not have one.');
+    }
 
  ?>
