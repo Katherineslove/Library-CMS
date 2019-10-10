2e8078b9b02a05e51c8bce2ff426ae1d465172d8
Katherineslove <48259743+Katherineslove@users.noreply.github.com>
2019-10-11 11:35:47 +1300

    mysql

--- 1902Library.sql
+++ 1902Library.sql
@@ -1,14 +1,20 @@
 -- phpMyAdmin SQL Dump
--- version 4.9.0.1
--- https://www.phpmyadmin.net/
+-- version 4.5.4.1deb2ubuntu2.1
+-- http://www.phpmyadmin.net
 --
--- Host: localhost:8889
--- Generation Time: Oct 10, 2019 at 08:17 PM
--- Server version: 5.7.26
--- PHP Version: 7.3.7
+-- Host: localhost
+-- Generation Time: Oct 10, 2019 at 10:35 PM
+-- Server version: 5.7.27-0ubuntu0.16.04.1
+-- PHP Version: 7.0.33-8+ubuntu16.04.1+deb.sury.org+1
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
 --
 -- Database: `1902Library`
@@ -48,39 +54,16 @@ CREATE TABLE `books` (
   `author_id` tinyint(6) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
--- --------------------------------------------------------
-
 --
--- Table structure for table `directors`
+-- Dumping data for table `books`
 --
 
-CREATE TABLE `directors` (
-  `_id` tinyint(6) NOT NULL,
-  `name` varchar(100) CHARACTER SET utf8mb4 NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
---
--- Dumping data for table `directors`
---
-
-INSERT INTO `directors` (`_id`, `name`) VALUES
-(1, '12344'),
-(2, '12344'),
-(3, '12344');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `movies`
---
-
-CREATE TABLE `movies` (
-  `_id` tinyint(6) UNSIGNED NOT NULL,
-  `title` varchar(100) CHARACTER SET utf8mb4 NOT NULL,
-  `year` year(4) NOT NULL,
-  `description` text CHARACTER SET utf8mb4 NOT NULL,
-  `director_id` tinyint(6) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+INSERT INTO `books` (`_id`, `title`, `year`, `description`, `author_id`) VALUES
+(1, 'test book', 2002, 'this is the description of our test book', 1),
+(2, 'Lion Witch and The Wardrobe', 2001, 'jkfdjdfjfjfjfsjdfg', 1),
+(3, 'Lion Witch and The Wardrobe', 2001, 'jkfdjdfjfjfjfsjdfg', 1),
+(4, 'Testing Book', 2002, 'Testing our book', 1),
+(5, 'Testing Book', 2002, 'Tesytkfkjhxfgkdfgkdfgk', 1);
 
 --
 -- Indexes for dumped tables
@@ -100,19 +83,6 @@ ALTER TABLE `books`
   ADD KEY `author_id` (`author_id`);
 
 --
--- Indexes for table `directors`
---
-ALTER TABLE `directors`
-  ADD PRIMARY KEY (`_id`);
-
---
--- Indexes for table `movies`
---
-ALTER TABLE `movies`
-  ADD PRIMARY KEY (`_id`),
-  ADD KEY `director_id` (`director_id`);
-
---
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -121,25 +91,11 @@ ALTER TABLE `movies`
 --
 ALTER TABLE `authors`
   MODIFY `_id` tinyint(6) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
-
 --
 -- AUTO_INCREMENT for table `books`
 --
 ALTER TABLE `books`
-  MODIFY `_id` tinyint(6) UNSIGNED NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `directors`
---
-ALTER TABLE `directors`
-  MODIFY `_id` tinyint(6) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
-
---
--- AUTO_INCREMENT for table `movies`
---
-ALTER TABLE `movies`
-  MODIFY `_id` tinyint(6) UNSIGNED NOT NULL AUTO_INCREMENT;
-
+  MODIFY `_id` tinyint(6) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 --
 -- Constraints for dumped tables
 --
@@ -150,8 +106,6 @@ ALTER TABLE `movies`
 ALTER TABLE `books`
   ADD CONSTRAINT `books_ibfk_1` FOREIGN KEY (`author_id`) REFERENCES `authors` (`_id`) ON UPDATE CASCADE;
 
---
--- Constraints for table `movies`
---
-ALTER TABLE `movies`
-  ADD CONSTRAINT `movies_ibfk_1` FOREIGN KEY (`director_id`) REFERENCES `directors` (`_id`) ON UPDATE CASCADE;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
